10a526d4c2d232d8b22857f89ab2d32ebc70ee4a
Juan <juand16@gmail.com>
2023-10-05 06:47:34 -0500

    add login controller, does not work yet

--- Security/security/src/main/java/com/ssmu/security/SecurityApplication.java
+++ Security/security/src/main/java/com/ssmu/security/SecurityApplication.java
@@ -3,7 +3,7 @@ package com.ssmu.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.ssmu.security" })
 public class SecurityApplication {
 
 	public static void main(String[] args) {
